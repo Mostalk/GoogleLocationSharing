3c71afac6d45d9b6edaa5e312c9fcae327a3de08
Mostalk <ivdifa.ka@gmail.com>
2021-12-02 19:36:20 +0300

    Initial commit

---
+++ GLC.py
@@ -0,0 +1,28 @@
+import configparser
+import json
+import requests
+
+
+class GLC:
+    def __init__(self, cookie, url):
+        self.url = url
+        self.header = {"cookie": cookie}
+        self.ulist = []
+        self.data = []
+
+    def req(self):
+        r = requests.get(self.url, headers=self.header)
+        self.data = json.loads(r.text.replace(")]}'", ""))[0]
+        return r
+
+    def get_user_list(self):
+        r = self.req()
+        for user in r:
+            self.ulist.append(user[0][3])
+        return self.ulist
+
+    def get_user_loc(self, name):
+        ulist = self.get_user_list()
+        for user in ulist:
+            if user[0][3] == name:
+                return user[1][4]
---
+++ main.py
@@ -0,0 +1,44 @@
+import configparser
+from GLC import GLC
+import telebot
+from telebot import types
+
+bot = telebot.TeleBot("", parse_mode="MARKDOWN")
+mm = types.ReplyKeyboardMarkup(row_width=2)
+button1 = types.KeyboardButton("Поиск")
+mm.add(button1)
+markup = types.InlineKeyboardMarkup()
+btn = types.InlineKeyboardButton(text='Отслеживать', callback_data="yes")
+markup.add(btn)
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+glc = GLC(config['main']['cookie'], config['main']['url'])
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    f = open("users", "a+", encoding='utf-8')
+    f.write(call.message.text + "\n")
+    f.close()
+
+
+@bot.message_handler(func=lambda m: True)
+def mess(message):
+    ulist = glc.get_user_list()
+    if message.text == "/start":
+        for user in ulist:
+            bot.send_message(message.chat.id, user, reply_markup=markup)
+    else:
+        f = open("users", encoding='utf-8').readlines()
+        ulist = ""
+        for user in f:
+            if glc.get_user_loc(user) == config['main']['adr']:
+                ulist += user
+            else:
+                bot.send_message(message.chat.id, "Никого нет", reply_markup=markup)
+            if ulist != "":
+                bot.send_message(message.chat.id, ulist, reply_markup=markup)
+
+
+bot.infinity_polling()
